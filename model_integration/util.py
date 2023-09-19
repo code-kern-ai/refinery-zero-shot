37ed189c8fe624b0948c32d5e2d02aeefc19816d
Felix Kirsch <125893779+FelixKirschKern@users.noreply.github.com>
2023-09-19 15:48:59 +0200

    new parent image (#47)
    * new parent image
    
    * formatting

--- model_integration/util.py
+++ model_integration/util.py
@@ -5,22 +5,19 @@ class ModelImplementation(Enum):
     GENERIC = "GENERIC"
 
 
-__lookup_model_implementation={
-    
-}
+__lookup_model_implementation = {}
 
-__lookup_model_hypothesis={
-    "sahajtomar/german_zeroshot":"In diesem geht es um {}."
-}
+__lookup_model_hypothesis = {"sahajtomar/german_zeroshot": "In diesem geht es um {}."}
 
-def lookup_implementation_routine(config:str):
+
+def lookup_implementation_routine(config: str):
     config = config.lower()
     if config in __lookup_model_implementation:
         return __lookup_model_implementation[config]
     return ModelImplementation.GENERIC
 
 
-def lookup_hypothesis(config:str):
+def lookup_hypothesis(config: str):
     config = config.lower()
     if config in __lookup_model_hypothesis:
         return __lookup_model_hypothesis[config]
